4d45e35971bf5fbd686bc6cbc32e02dedbe29306
Josh Roth <you@example.comraider4414@gmail.com>
2024-03-05 19:05:38 -0600

    updated index.js

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,55 @@
+
+const typeDefs = `
+  type Book {
+    _id: ID!
+    authors: [String]!
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]!
+    bookCount: Int!
+  }
+
+  input BookInput {
+    authors: [String]!
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  input UserInput {
+    username: String!
+    email: String!
+    password: String!
+  }
+
+  type Auth {
+    token: String!
+    user: User!
+  }
+
+  type Query {
+    getUser(_id: ID!): User
+    me: User
+  }
+
+  type Mutation {
+    createUser(userInput: UserInput!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(bookInput: BookInput!): User
+    deleteBook(bookId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
