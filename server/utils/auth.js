f196d365f6b218afb17a2814440a6aeb4342a864
Josh Roth <you@example.comraider4414@gmail.com>
2024-03-05 18:06:38 -0600

    Auth.js updated for GraphQL

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,35 +1,25 @@
 const jwt = require('jsonwebtoken');
 
-// set token secret and expiration date
+// set token secret and expiration date needed.
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
-
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
+  // authenticated route function
+  authMiddleware: function (context) {
+    const token = context.headers.authorization;
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      throw new Error('You have no token!');
     }
 
-    // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
+      context.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      throw new Error('Invalid token!');
     }
-
-    // send to next endpoint
-    next();
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
