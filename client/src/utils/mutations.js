a1df37780c7e6a439aa344a0c83bb008d6655c77
Josh Roth <you@example.comraider4414@gmail.com>
2024-03-05 19:44:08 -0600

    seperated Muts from qrys

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,57 @@
+import { gql } from '@apollo/client';
+// mutations
+export const CREATE_USER = gql`
+  mutation CreateUser($username: String!, $email: String!, $password: String!) {
+    createUser(username: $username, email: $email, password: $password) {
+      token
+    }
+  }
+`;
+
+export const LOGIN = gql`
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation SaveBook($bookInput: BookInput!) {
+    saveBook(bookInput: $bookInput) {
+      _id
+      username
+      email
+      savedBooks {
+        _id
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
+      bookCount
+    }
+  }
+`;
+
+export const DELETE_BOOK = gql`
+  mutation DeleteBook($bookId: ID!) {
+    deleteBook(bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        _id
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
+      bookCount
+    }
+  }
+`;
